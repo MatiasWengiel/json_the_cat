e502886c0c8b3e8c6ae7d17f123e109eda10d493
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-22 09:30:45 -0500

    Program requests breed information for Siberian cats and logs the description

---
+++ breedFetcher.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=sib`, (error, response, body) => {
+  if (error) {console.log('error: ', error)};
+  if (response.statusCode !== 200) {console.log('statusCode: ', response && response.statusCode)};
+  const data = JSON.parse(body);
+  console.log(data[0].description);
+})
