677afec17ca2bb3c13e795418647b44d80e5721d
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-22 11:21:32 -0500

    Created separate file for running breedFetcher function, and refactored to increase modularity and testability

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,31 +1,33 @@
 const request = require('request');
 
-const breed = process.argv.slice(2);
-
 const errorHandling = (error, response) => {
   if (error) {
-    console.log('error: ', error);
     return 'error';
   }
 
   if (response.statusCode !== 200) {
-    console.log('statusCode: ', response && response.statusCode);
     return 'error';
   }
 };
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-
-  if (errorHandling(error, response) === 'error') {
-    return;
-  }
-
-  if (body === '[]') {
-    console.log('The search parameter did not return any breeds');
-  } else {
-    const data = JSON.parse(body);
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     
-    console.log(data[0].description);
-  }
+    if (errorHandling(error, response) === 'error') {
+      callback(error, null);
+    }
   
-});
+
+    if (body === '[]') {
+      callback('The search parameter did not return any breeds', null);
+    } else {
+      const data = JSON.parse(body);
+      
+      callback(null, data[0].description);
+    }
+    
+  });
+
+};
+
+module.exports = {fetchBreedDescription};
