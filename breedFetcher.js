a638cd7f7fe32a616d7e3bd5e15cbfc4160177fc
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-22 10:13:25 -0500

    Program now returns breed description from CLI argument search if found, and notifies user if no breeds were returned. Also prints to console error information if present, and request status information if not successful (i.e. 200)

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,9 +2,30 @@ const request = require('request');
 
 const breed = process.argv.slice(2);
 
+const errorHandling = (error, response) => {
+  if (error) {
+    console.log('error: ', error);
+    return 'error';
+  }
+
+  if (response.statusCode !== 200) {
+    console.log('statusCode: ', response && response.statusCode);
+    return 'error';
+  }
+};
+
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {console.log('error: ', error)};
-  if (response.statusCode !== 200) {console.log('statusCode: ', response && response.statusCode)};
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-})
+
+  if (errorHandling(error, response) === 'error') {
+    return;
+  }
+
+  if (body === '[]') {
+    console.log('The search parameter did not return any breeds');
+  } else {
+    const data = JSON.parse(body);
+    
+    console.log(data[0].description);
+  }
+  
+});
